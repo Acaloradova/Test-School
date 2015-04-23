d1cc68db4bcfe09dfd1e874a51b9e091c19999c3
Julik <julik@atlygin.com>
2015-04-23 21:53:29 +0300

    Changes in Perimeter.java: calculation for circle and square have been added

--- src/task4_Perimeter/Perimeter.java
+++ src/task4_Perimeter/Perimeter.java
@@ -3,26 +3,37 @@ package task4_Perimeter;
 /**
  * Created by julik on 4/23/15.
  */
+import java.util.Scanner;
 
 public class Perimeter {
+
+    public static final double PI = 3.141596;
+
     public static void main(String[] args) {
-        //System.out.println("We are about perimeter calculation. Write triangle, square or circle");
-        Circle circle = new Circle(321);
-        System.out.println(circle.getPerimeter());
-    }
-    static interface Shape{
-        public double getPerimeter();
+        Scanner in = new Scanner(System.in);
+        String shape = in.next();
+        double side = in.nextDouble();
+        double perimeter = 0;
+        if (shape.equals("circle")) {
+            Circle circle = new Circle(side);
+            perimeter = circle.getPerimeter();
+        } else if (shape.equals("square")) {
+            perimeter = 4 * side;
+        }
+        System.out.println("perimeter of " + shape + " with side/radius " + side + " is " + perimeter);
     }
 
-    static class Circle implements Shape {
-        double r;
-        public Circle(double radius) {
-            r = radius;
+    public static class Circle {
+        double radius;
+
+        Circle(double radius) {
+            this.radius = radius;
         }
 
-        public double getPerimeter(){
-            return 0;
+        double getPerimeter() {
+            return 2 * PI * radius;
         }
     }
+
 }
 
